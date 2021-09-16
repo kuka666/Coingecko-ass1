af565ac7d1c965d8a2c20db2d3b77db3610dc06c
kuka666 <kuka666@github.com>
2021-09-16 23:51:22 +0600

    hello

---
+++ src/test.py
@@ -0,0 +1,18 @@
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+
+
+def print_coin(Btc):
+    print(Btc['current_price'], "price:", Btc['current_price'], "$")
+
+
+def cap_sort(e):
+    return e['market_cap_rank']
+
+
+for i in range(1):
+    n = int(input("Number to output top: "))
+    coin_list = cg.get_coins_markets("usd")[:n]
+    coin_list.sort(key=cap_sort)
+    for coin in coin_list:
+        print_coin(coin)
