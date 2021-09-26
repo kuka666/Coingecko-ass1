b8cb0321de4f4c30c930a91b6cded8f50081d757
jessssssssssss <80199144+kuka666@users.noreply.github.com>
2021-09-26 15:11:01 +0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,17 +2,13 @@ from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
 
-def crypto_sort(a):
-    return a['market_cap_rank']
-
 
 def out_crypto(Btc):
-    print(Btc['symbol'], "price:", Btc['current_price'], "$")
+    print(Btc['name']+" "+Btc['symbol'], "price:", Btc['current_price'], "$")
 
 
 for i in range(1):
     b = int(input("Number to output top: "))
     list = cg.get_coins_markets("usd")[:b]
-    list.sort(key=crypto_sort)
     for crypto in list:
         out_crypto(crypto)
